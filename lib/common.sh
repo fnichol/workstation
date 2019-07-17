08f9327a26679703d262e8f175e4eb3eee9ac54c
Fletcher Nichol <fnichol@nichol.ca>
2019-07-17 11:14:14 -0600

    [common] Add mktemp support for old macOS 10.10
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- lib/common.sh
+++ lib/common.sh
@@ -170,12 +170,11 @@ keep_sudo() {
   done 2>/dev/null &
 }
 
-# Looks like the maximally portable way of calling `mktemp` to create a file is
-# to provide no arguments (therefore having no control over the naming). All
-# tested invocations will create a file in each platform's suitable temporary
-# directory.
+# Most systems use the first no-argument version, however Mac OS X 10.10
+# (Yosemite) and older don't allow the no-argument version, hence the second
+# fallback version.
 mktemp_file() {
-  mktemp
+  mktemp 2>/dev/null || mktemp -t tmp
 }
 
 need_cmd() {
