9f1979dcbfb2993845f526ad3354ccd5c2cb7328
Fletcher Nichol <fnichol@nichol.ca>
2020-11-01 10:53:15 -0700

    [freebsd] Extract business logic into `freebsd_*` functions
    Closes #11
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- lib/freebsd.sh
+++ lib/freebsd.sh
@@ -1,6 +1,49 @@
 #!/usr/bin/env sh
 # shellcheck disable=SC2039
 
+freebsd_set_hostname() {
+  local fqdn="$1"
+
+  need_cmd grep
+
+  sudo hostname "$fqdn"
+
+  if grep -q '^hostname=' /etc/rc.conf >/dev/null; then
+    need_cmd sed
+    sudo sed -i '' "s/^hostname=.*$/hostname=\"$fqdn\"/" /etc/rc.conf
+  else
+    need_cmd tee
+    echo "hostname=\"$fqdn\"" | sudo tee -a /etc/rc.conf >/dev/null
+  fi
+}
+
+freebsd_setup_package_system() {
+  indent sudo pkg update
+}
+
+freebsd_update_system() {
+  indent sudo pkg upgrade --yes --no-repo-update
+}
+
+freebsd_install_base_packages() {
+  local data_path="$1"
+
+  install_pkg jq
+  install_pkgs_from_json "$data_path/freebsd_base_pkgs.json"
+}
+
+freebsd_install_headless_packages() {
+  local data_path="$1"
+
+  install_pkgs_from_json "$data_path/freebsd_headless_pkgs.json"
+  freebsd_install_beets
+}
+
+freebsd_install_node() {
+  install_pkg node
+  install_pkg npm
+}
+
 freebsd_install_pkg() {
   need_cmd pkg
 
@@ -15,10 +58,30 @@ freebsd_install_pkg() {
 }
 
 freebsd_install_beets() {
-  install_pkg py36-pip
-  install_pkg py36-gdbm
+  need_cmd uname
+
+  local release
+  release="$(uname -r)"
+
+  case "$release" in
+    12.*)
+      install_pkg py37-pip
+      install_pkg py37-gdbm
+      ;;
+    11.*)
+      install_pkg py36-pip
+      install_pkg py36-gdbm
+      ;;
+    *)
+      warn "Installing beets on $release no yet supported, skipping"
+      ;;
+  esac
+
   install_pkg ffmpeg
-  install_pkg lame
+
+  # TODO: support compiling lame pkg
+  # See: fnichol/workstation#15
+  # install_pkg lame
 
   install_beets_pip_pkgs
 }
