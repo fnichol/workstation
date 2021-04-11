d2f88968346de99d32c37ed3eab94ddb6f108a86
Fletcher Nichol <fnichol@nichol.ca>
2021-04-11 01:11:26 -0600

    Upgrade to vendored libsh 0.8.0
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- vendor/mk/libsh.mk
+++ vendor/mk/libsh.mk
@@ -1,6 +1,6 @@
 vendor-libsh: ## Vendors updated version of libsh
 	@echo "--- $@"
 	curl --proto '=https' --tlsv1.2 -sSf \
-		https://raw.githubusercontent.com/fnichol/libsh/master/install.sh \
+		https://fnichol.github.io/libsh/install.sh \
 		| sh -s -- --mode=vendor --release=latest
 .PHONY: vendor-libsh
